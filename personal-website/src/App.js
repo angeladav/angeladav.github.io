5d65799220d00d64253ac1ae7771508cc2b9fa18
Angela Davis <angeladavis2024@u.northwestern.edu>
2024-06-26 15:32:40 -0400

    finished navbar

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -3,14 +3,19 @@ import { Routes, Route } from 'react-router-dom';
 import AboutMe from './pages/AboutMe'
 import Home from './pages/Home'
 import Resume from './pages/Resume'
+import NavBar from './pages/NavBar'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/aboutme" element={<AboutMe />} />
-      <Route path="/resume" element={<Resume />} />
-    </Routes>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutme" element={<AboutMe />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+    </>
+
   );
 }
 
---
+++ personal-website/src/pages/NavBar.js
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import '../styling/NavBar.css'
+
+function NavBar() {
+    return (
+        <div className="navbar">
+            <div className="navbar-page-container">
+                <Link className="navbar-page-link" to="/">
+                    <button className="navbar-page-button">
+                        Home
+                    </button>
+                </Link>
+                <Link className="navbar-page-link" to="/aboutme">
+                    <button className="navbar-page-button">
+                        About Me
+                    </button>
+                </Link>
+                <Link className="navbar-page-link" to="/resume">
+                    <button className="navbar-page-button">
+                        Resume
+                    </button>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
